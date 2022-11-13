38efef4072b0a4214abb2b234b8b2cb687d3a318
Sergey_Shteyman <Shteiman10@gmail.com>
2022-11-14 00:05:09 +0300

    Add transfer for tests from product page to basket page

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,6 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
-from time import sleep
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -21,6 +20,5 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.open()
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
-    sleep(3)
     basket_page.basket_should_be_empty()
     basket_page.basket_empty_message()
