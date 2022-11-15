17433a82198ce198a9d95a13d4f8f1e8edbd6d43
Sergey_Shteyman <Shteiman10@gmail.com>
2022-11-15 21:43:24 +0300

    Prepare code to code review

--- test_product_page.py
+++ test_product_page.py
@@ -17,20 +17,24 @@ class TestUserAddToBasketFromProductPage:
         login_page.register_new_user(login, password)
         login_page.should_be_authorized_user()
 
-    urls = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-            pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                         marks=pytest.mark.xfail),
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
+        # На всякий случай оставил парметризацию. С ней тоже все тесты проходят.
 
-    @pytest.mark.parametrize('link', urls)
-    def test_guest_can_add_product_to_basket(self, browser, link):
+    # urls = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+    #         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+    #         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+    #         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+    #         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+    #         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+    #         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+    #         pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+    #                      marks=pytest.mark.xfail),
+    #         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+    #         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
+    #
+    # @pytest.mark.parametrize('link', urls)
+    @pytest.mark.need_review
+    def test_guest_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         product_page = ProductPage(browser, link)
         product_page.open()
         product_page.should_be_add_product_to_the_basket_button()
@@ -44,6 +48,7 @@ class TestUserAddToBasketFromProductPage:
         product_page.open()
         product_page.should_be_login_link()
 
+    @pytest.mark.need_review
     def test_guest_can_go_to_login_page_from_product_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         product_page = ProductPage(browser, link)
@@ -52,6 +57,7 @@ class TestUserAddToBasketFromProductPage:
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
+    @pytest.mark.need_review
     def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         product_page = ProductPage(browser, link)
@@ -61,6 +67,7 @@ class TestUserAddToBasketFromProductPage:
         basket_page.basket_should_be_empty()
         basket_page.basket_empty_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         product_page = ProductPage(browser, link)
